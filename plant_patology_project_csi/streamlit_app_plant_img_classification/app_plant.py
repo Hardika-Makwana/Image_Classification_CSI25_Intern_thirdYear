e08b9fb6a568fbe1026fb127371a5a5e78fea89c
Hardika-Makwana <166051037+Hardika-Makwana@users.noreply.github.com>
2025-07-23 15:06:13 +0530

    Add new Streamlit app, removed old app_plant.py

--- plant_patology_project_csi/streamlit_app_plant_img_classification/app_plant.py
+++
@@ -1,9 +0,0 @@
-import joblib
-import os
-
-current_dir = os.path.dirname(__file__)   # This is streamlit-app/
-model_path = os.path.join(current_dir, "gbm_model_plant_csi.pkl")
-
-gbm_model = joblib.load(model_path)
-print("Model loaded successfully!")
-
